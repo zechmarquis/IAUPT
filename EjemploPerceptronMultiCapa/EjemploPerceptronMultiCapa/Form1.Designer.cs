9cf4874771fc2c3669c5e2f79bab79dc957e2f1b
zechmarquis <albertoll06@hotmail.com>
2019-08-08 00:40:41 -0500

    Add files via upload

---
+++ EjemploPerceptronMultiCapa/EjemploPerceptronMultiCapa/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace EjemploPerceptronMultiCapa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEntrenar = new System.Windows.Forms.Button();
+            this.btnReconocer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEntrenar
+            // 
+            this.btnEntrenar.Location = new System.Drawing.Point(51, 33);
+            this.btnEntrenar.Name = "btnEntrenar";
+            this.btnEntrenar.Size = new System.Drawing.Size(75, 23);
+            this.btnEntrenar.TabIndex = 0;
+            this.btnEntrenar.Text = "Entrenar";
+            this.btnEntrenar.UseVisualStyleBackColor = true;
+            this.btnEntrenar.Click += new System.EventHandler(this.BtnEntrenar_Click);
+            // 
+            // btnReconocer
+            // 
+            this.btnReconocer.Location = new System.Drawing.Point(51, 92);
+            this.btnReconocer.Name = "btnReconocer";
+            this.btnReconocer.Size = new System.Drawing.Size(75, 23);
+            this.btnReconocer.TabIndex = 1;
+            this.btnReconocer.Text = "Reconocer";
+            this.btnReconocer.UseVisualStyleBackColor = true;
+            this.btnReconocer.Click += new System.EventHandler(this.BtnReconocer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(168, 136);
+            this.Controls.Add(this.btnReconocer);
+            this.Controls.Add(this.btnEntrenar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEntrenar;
+        private System.Windows.Forms.Button btnReconocer;
+    }
+}
+
---
+++ EjemploPerceptronMultiCapa/EjemploPerceptronMultiCapa/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IAupt.uptRNA;
+
+namespace EjemploPerceptronMultiCapa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEntrenar_Click(object sender, EventArgs e)
+        {
+            PerceptronMultiCapa rna = new PerceptronMultiCapa(@"ejemplo.pml");
+            rna.entrenar();
+        }
+
+        private void BtnReconocer_Click(object sender, EventArgs e)
+        {
+            PerceptronMultiCapa rna = new PerceptronMultiCapa(@"ejemplo.ppm");
+
+            double[] x = { 1, 0, 1 };
+            rna.reconocer(x);
+
+            double [,] y = rna.y;
+        }
+    }
+}
